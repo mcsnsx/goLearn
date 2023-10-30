79e3572be2171c9fa1c66dde6bf91975b85dcec0
mcsnsx <mcs_nasc@hotmail.com>
2023-10-30 10:34:18 -0300

    panic e recover

---
+++ 14 - Funções Avançadas/14.6 - Panic e Recover/panic-recover.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	//fmt.Println("Tentativa de recupera a execução")
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso")
+	}
+
+}
+
+func alunoAprovado(n1, n2 float32) bool {
+	defer recuperarExecucao()
+	fmt.Println("Função aluno aprovado")
+
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+	//a função panic interrompe o fluxo normal do seu programa quando retorna algo inesperado e vai parar o processo, ele vai "entrar em panico"
+	//ela vai chamar todas as funções que tem defer se você não chamar o recover e o programa morre
+	//o erro pode ser tratado e continuar a execussão do programa, o panic interrompe e mata o programa e nada
+	//que vem a seguir é executado, o programa só pode ser recuperado usando a clausula recover
+	panic("A media é exatamente 6!")
+}
+
+func main() {
+	fmt.Println(alunoAprovado(6, 6))
+	fmt.Println("Pós execução")
+}
