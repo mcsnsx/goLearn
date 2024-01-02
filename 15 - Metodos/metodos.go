542bf645523cd468c249fd8271309c3e826016bb
mcsnsx <mcs_nasc@hotmail.com>
2024-01-02 10:39:57 -0300

    Métodos

--- 15 - Metodos/metodos.go
+++ 15 - Metodos/metodos.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// MÉTODOS
+
 // a diferença entre metodo e função é que o metodo ta sempre associado a alguma coisa (struct, interface, etc)
 // cria-se metodos para dar ações as funções, é algo parecido com a programação orientada a objetos onde vc
 // tem as classes, os atributos e os objetos
