798b1112c9492bdbfb348ae3f54c4a61d7c9380b
mcsnsx <mcs_nasc@hotmail.com>
2024-01-03 11:48:10 -0300

    Aplicação de linha de comando - cmd

--- 17 - Aplicação de Linha de Comando/app/app.go
+++ 17 - Aplicação de Linha de Comando/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Essa funçaõ retorna a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
@@ -8,5 +14,56 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "criticalcloud.io",
+		},
+	}
+
+	//é um slice de comando que nossa aplicação vai entender e conseguir executar
+	//é um tipo especifico de slice cli porque podemos ter mais de um comando na nossa aplicação
+	//o tipo 'Command' é um struct
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host) //name server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
