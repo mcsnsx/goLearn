d113cb0b1e7459c97e240acd005f889108af0117
mcsnsx <mcs_nasc@hotmail.com>
2024-01-04 09:51:56 -0300

    Padrão de concorrencia - worker pools

--- 18 - Concorrencia/padroes-concorrencia/WorkerPools/worker-pools.go
+++ 18 - Concorrencia/padroes-concorrencia/WorkerPools/worker-pools.go
@@ -1,11 +1,28 @@
 package main
 
+import "fmt"
+
 // Worker Pools --> é como se você tivesse uma fila de tarefaas para serem executadas e você tem vários workers
 // vários processos pegando itens dessa fila de maneira independente
 
 func main() {
 	tarefas := make(chan int, 45)
 	resultados := make(chan int, 45)
+
+	//para diminuir o tempo de execução, podemos chamar as goroutines mais de uma vez
+	go worker(tarefas, resultados)
+	go worker(tarefas, resultados)
+	go worker(tarefas, resultados)
+
+	for i := 0; i < 45; i++ {
+		tarefas <- i
+	}
+	close(tarefas)
+
+	for i := 0; i < 45; i++ {
+		resultado := <-resultados
+		fmt.Println(resultado)
+	}
 }
 
 // nesse caso é possivel especificar se o canal envia (chan<-) ou recebe (<-chan) dados
