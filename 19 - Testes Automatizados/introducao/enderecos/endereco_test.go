9f32806c08a182b19dea3e363ba7a796fa7c24db
mcsnsx <mcs_nasc@hotmail.com>
2024-01-05 09:31:50 -0300

    Testes Unitários com diversos cenários

--- 19 - Testes Automatizados/introducao/enderecos/endereco_test.go
+++ 19 - Testes Automatizados/introducao/enderecos/endereco_test.go
@@ -12,18 +12,41 @@ import "testing"
 // Os testes são a unica excessão onde podemos ter mais de um pacote dentro da mesma
 // pasta/diretorio
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 // O nome da função de teste tem que começar com 'Test' e o nome da função que será testada
 // ex: TestXxxxxxx, esse função deve receber um ponteiro como parametro, do tipo T
 func TestTipoDeEndereco(t *testing.T) {
-	enderecoParaTeste := "Rua ABC"
-	tipoDeEnderecoEsperado := "Avenida"
-	tipoDeEnderecoRecebido := TipoEndereco(enderecoParaTeste)
+	// enderecoParaTeste := "Rua ABC"
+	// tipoDeEnderecoEsperado := "Avenida"
+	// tipoDeEnderecoRecebido := TipoEndereco(enderecoParaTeste)
+
+	// essa é uma forma de testar várias respostas possiveis em um mesmo slice
+	cenarioDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "tipo invalido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"", "tipo invalido"},
+	}
+
+	for _, cenario := range cenarioDeTeste {
+		retornoRecebido := TipoEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", retornoRecebido, cenario.retornoEsperado)
+		}
+	}
 
 	// Se ele cair aqui, é dito que o "teste quebrou", se ele não cair aqui, é porque
 	// o "teste passou"
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
-	}
+	// 	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
+	// 		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	// 	}
 }
 
 // Esse teste não é considerado muito confiável porque está testando apenas um caso,
