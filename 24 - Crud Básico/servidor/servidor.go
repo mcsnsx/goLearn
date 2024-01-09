4c980e1739cc44caa55a35f96bbc57eb4cdbbd9f
mcsnsx <mcs_nasc@hotmail.com>
2024-01-09 11:12:38 -0300

    Buscando Dados

--- 24 - Crud Básico/servidor/servidor.go
+++ 24 - Crud Básico/servidor/servidor.go
@@ -65,3 +65,11 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
 }
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
